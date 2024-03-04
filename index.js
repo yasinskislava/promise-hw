12468a7d0c8858ae38e69ae0e3b60730175c46ca
yasinskislava <yasinskislava@gmail.com>
2024-03-04 20:54:17 +0200

    save

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const delay = ms => {
-    const promise = new Promise((res, rej) => {
-        setTimeout(() => { res(ms) }, ms);
-    });
-    return promise;
+const delay = (ms) => {
+  const promise = new Promise((res, rej) => {
+    setTimeout(() => {
+      res(ms);
+    }, ms);
+  });
+  return promise;
 };
 
-const logger = time => console.log(`Resolved after ${time}ms`);
+const logger = (time) => console.log(`Resolved after ${time}ms`);
 
 // Виклич функції для перевірки
 delay(2000).then(logger); // Resolved after 2000ms
@@ -15,23 +17,23 @@ delay(1500).then(logger); // Resolved after 1500ms
 //////////////////////////////////////////////////////////////////////
 
 const users = [
-  { name: 'Mango', active: true },
-  { name: 'Poly', active: false },
-  { name: 'Ajax', active: true },
-  { name: 'Lux', active: false },
+  { name: "Mango", active: true },
+  { name: "Poly", active: false },
+  { name: "Ajax", active: true },
+  { name: "Lux", active: false },
 ];
 
 const toggleUserState = (allUsers, userName) => {
-    const updatedUsers = allUsers.map(user =>
-        user.name === userName ? { ...user, active: !user.active } : user
-    );
-    const promise = new Promise((resolve, reject) => {
-        resolve(updatedUsers);
-    });
-    return promise;
+  const updatedUsers = allUsers.map((user) =>
+    user.name === userName ? { ...user, active: !user.active } : user
+  );
+  const promise = new Promise((resolve, reject) => {
+    resolve(updatedUsers);
+  });
+  return promise;
 };
 
-const logger1 = updatedUsers => console.table(updatedUsers);
+const logger1 = (updatedUsers) => console.table(updatedUsers);
 /*
  * Зараз працює так
  */
@@ -41,8 +43,8 @@ const logger1 = updatedUsers => console.table(updatedUsers);
 /*
  * Повинно працювати так
  */
-toggleUserState(users, 'Mango').then(logger1);
-toggleUserState(users, 'Lux').then(logger1);
+toggleUserState(users, "Mango").then(logger1);
+toggleUserState(users, "Lux").then(logger1);
 
 // //////////////////////////////////////////////////////////////////////
 
@@ -51,25 +53,25 @@ const randomIntegerFromInterval = (min, max) => {
 };
 
 const makeTransaction = (transaction) => {
-    const delay = randomIntegerFromInterval(200, 500);
-    const promise = new Promise((resolve, reject) => {
-        setTimeout(() => {
-          const canProcess = Math.random() > 0.3;
-          if (canProcess) {
-            resolve(transaction.id, delay);
-          } else {
-            reject(transaction.id);
-          }
-        }, delay);
-    });
-    return promise;
+  const delay = randomIntegerFromInterval(200, 500);
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const canProcess = Math.random() > 0.3;
+      if (canProcess) {
+        resolve([transaction.id, delay]);
+      } else {
+        reject(transaction.id);
+      }
+    }, delay);
+  });
+  return promise;
 };
 
-const logSuccess = (id, time) => {
+const logSuccess = ([id, time]) => {
   console.log(`Transaction ${id} processed in ${time}ms`);
 };
 
-const logError = id => {
+const logError = (id) => {
   console.warn(`Error processing transaction ${id}. Please try again later.`);
 };
 
@@ -83,18 +85,10 @@ const logError = id => {
 /*
  * Повинно працювати так
  */
-makeTransaction({ id: 70, amount: 150 })
-  .then(logSuccess)
-  .catch(logError);
+makeTransaction({ id: 70, amount: 150 }).then(logSuccess).catch(logError);
 
-makeTransaction({ id: 71, amount: 230 })
-  .then(logSuccess)
-  .catch(logError);
+makeTransaction({ id: 71, amount: 230 }).then(logSuccess).catch(logError);
 
-makeTransaction({ id: 72, amount: 75 })
-  .then(logSuccess)
-  .catch(logError);
+makeTransaction({ id: 72, amount: 75 }).then(logSuccess).catch(logError);
 
-makeTransaction({ id: 73, amount: 100 })
-  .then(logSuccess)
-  .catch(logError);
+makeTransaction({ id: 73, amount: 100 }).then(logSuccess).catch(logError);
